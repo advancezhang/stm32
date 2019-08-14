559592b761805a5a5f171ddb40775b51879cac85
Advancezhang <1830444538@qq.com>
2019-08-14 16:26:14 +0800

    7

--- USER/main.c
+++ USER/main.c
@@ -218,9 +218,9 @@ typedef struct
 }PID;
 
 
-#define  P_DATA      -1.5                                 //P参数
-#define  I_DATA      0.8                                //I参数
-#define  D_DATA      0.4                               //D参数
+float P_DATA=-1.5;                               //P参数
+float I_DATA=0.8;                                //I参数
+float D_DATA=0.4;                               //D参数
 
 __IO uint16_t dcmotor_speed=3000;
 __IO double encoder_speed=0;
@@ -257,7 +257,6 @@ extern u16	TIM4CH1_CAPTURE_VAL;	//输入捕获值
 void lcdspeed(void);
 void drawspeed(void);
 
-u8 actual=50;
 u16 adcx;
 float temp;
 float angle;
@@ -334,7 +333,10 @@ void drawspeed(void)
 	for(a=0;a<=35;a++)
 	{
 		LCD_DrawLine(45,a*5+500,33,a*5+500);
+		LCD_DrawLine(45,590,28,590);
 	}
+	sprintf((char*)str,"%d",sptr->SetPoint);
+	LCD_ShowString(95,160,85,40,24,str);
 	LCD_ShowString(15,35,85,40,24,"Speed:");
 	LCD_ShowString(245,35,85,40,24,"Angle:");
 	LCD_ShowString(15,135,65,40,24,"Set   Speed:");
@@ -342,10 +344,9 @@ void drawspeed(void)
 	LCD_ShowString(20,450,85,40,24,"0");
 	LCD_ShowString(0,340,85,40,24,"50");
 	LCD_ShowString(0,240,85,40,24,"100");
-	LCD_ShowString(95,160,85,40,24,"50");
-	LCD_ShowString(20,670,60,30,24,"0");
-	LCD_ShowString(0,575,60,30,24,"180");
-	LCD_ShowString(0,485,60,30,24,"360");
+	LCD_ShowString(20,670,60,30,16,"0");
+	LCD_ShowString(0,580,60,30,16,"180");
+	LCD_ShowString(0,485,60,30,16,"360");
 	LCD_ShowString(15,720,77,40,24,"KP");
 	LCD_ShowString(169,720,77,40,24,"KI");
 	LCD_ShowString(323,720,77,40,24,"KD");
